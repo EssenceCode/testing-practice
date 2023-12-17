1bb0e15fadf09612532c5d07f417cea05f4ec739
EssenceCode <123456789+odin@users.noreply.github.com>
2023-12-17 14:15:29 +0800

    created a test for a calculator

---
+++ src/modules/calculator/calculator.test.js
@@ -0,0 +1,17 @@
+const Calculator = require("./calculator");
+
+test("adds 2 + 2 to equal to 4", () => {
+    expect(Calculator.add(2, 2)).toBe(4);
+});
+
+test("subtract 4 - 2 to equal to 2", () => {
+    expect(Calculator.subtract(4, 2)).toBe(2);
+})
+
+test("divide 4 / 2 to equal to 2", () => {
+    expect(Calculator.divide(4, 2)).toBe(2);
+})
+
+test("multiply 4 * 2 to equal to 8", () => {
+    expect(Calculator.multiply(4, 2)).toBe(8);
+})
