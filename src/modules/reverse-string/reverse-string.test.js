075ce8be35d5804d75555a32696170c83bf612a3
EssenceCode <123456789+odin@users.noreply.github.com>
2023-12-17 14:05:54 +0800

    a test for a function that takes and reversed its returned string

---
+++ src/modules/reverse-string/reverse-string.test.js
@@ -0,0 +1,9 @@
+const reverseString = require("./reverse-string")
+
+test("takes a string and reverse it", () => {
+    expect(reverseString("car")).toBe("rac");
+})
+
+test("reverse orange to egnaro", () => {
+    expect(reverseString("orange")).toBe("egnaro");
+})
