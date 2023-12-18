7f48f20af9bf1f9836f5de858ff11805fab5ec39
EssenceCode <123456789+odin@users.noreply.github.com>
2023-12-18 14:42:26 +0800

    add test for special character

--- src/modules/caesar-cipher/caesarCipher.test.js
+++ src/modules/caesar-cipher/caesarCipher.test.js
@@ -1,21 +1,40 @@
+// const caesarCipher = require("./caesarCipher");
+
 const caesarCipher = require("./caesarCipher");
 
-test("can shift from a to z ", () => {
-    expect(caesarCipher("a", 1)).toBe("b");
+test("returns empty string if there's no given string as argument", () => {
+    expect(caesarCipher("", 1)).toBe("");
 });
 
-test("can shift from z to a ", () => {
-    expect(caesarCipher("z", 1)).toBe("a");
+test("returns the string if there's no shift value given", () => {
+    expect(caesarCipher("abc")).toBe("abc");
+});
+
+test("works with smaller case", () => {
+    expect(caesarCipher("abc", 1)).toBe("bcd")
+});
+
+test("wraps smaller case from z to a", () => {
+    expect(caesarCipher("xyz", 1)).toBe("yza")
+});
+
+test("works with special characters", () => {
+    expect(caesarCipher("@abc", 1)).toBe("@bcd")
+});
+
+test("works with punctuation and smaller case", () => {
+    expect(caesarCipher("xyz!", 1)).toBe("yza!")
 });
 
 test("works with capital letters", () => {
-    expect(caesarCipher("Z", 1)).toBe("A");
+    expect(caesarCipher("ABC", 1)).toBe("BCD")
 });
 
-test("works if string includes punctuation", () => {
-    expect(caesarCipher("Z.", 1)).toBe("A.");
+test("wraps upper case from Z to A", () => {
+    expect(caesarCipher("XYZ", 1)).toBe("YZA")
 });
 
-test("works if string includes punctuation", () => {
-    expect(caesarCipher("Z!", 1)).toBe("A!");
+test("works with punctuation and upper case", () => {
+    expect(caesarCipher("XYZ!", 1)).toBe("YZA!")
 });
+
